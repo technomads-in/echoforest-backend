71904dbfa4e95b6d84d8df0d24df8d2f70d055ba
Nodejs-Dev <ruchit.15801@gmail.com>
2022-10-18 16:01:17 +0530

    add email

--- src/controller/main.js
+++ src/controller/main.js
@@ -1,5 +1,7 @@
 const newsLetter = require("../model/newsLetter");
 const contact = require("../model/contactUs");
+const {sendEmail, sendWelcomeEmail} = require("../utils/email");
+
 
 const newsletter = async(req,res) => {
     try{
@@ -40,7 +42,31 @@ const contactUs = async(req, res) => {
           return res.status(400).send({ status: false, message: "Data not found" });
         }
 
-        return res.status(200).send({ status: true, data: user });
+
+        var sendMailData = {
+          "file_template": './public/EmailTemplates/welcome.html',
+          // "to": email ? email : null,
+          "username": `${ name }`,
+          "from": email,
+          "message": message
+      }
+  
+      sendEmail(sendMailData).then((val) => {
+          return res.status(200).send({ 'status': true, 'code': 200, 'message': "Please check your email.", 'data': val })
+      }).catch((err) => {
+          console.log(err);
+          return res.status(401).send({ "status": false, 'code': 200, "message": "Unable to send email!", data: {} })
+      })
+  
+  
+      // without tempalte 
+      // send welcome email
+              // var subject1 = "Welcome to Ecoforest"
+              // var html1 = "<h2>Hello , <br>Congratulations! You have successfully signed up for Ecoforest  </br> We are super excited to welcome you to our Ecoforest Community.</br> Now you are all set to buy our WIZZ NFT Smart Node to start earning passive income.</br> Thank You,</br> Team Wizz"
+  
+              // var mail = await sendWelcomeEmail(email, subject1, html1)
+  
+      return res.status(200).send({ status: true, data: user });
     }catch(e) {
         console.log(e);
         res.status(500).json({
---
+++ src/utils/email.js
@@ -0,0 +1,102 @@
+const nodemailer = require('nodemailer')
+const fs = require('file-system')
+const handlebars = require('handlebars')
+
+// send welcome email template
+const sendEmail = (sendData) => {
+    try{
+
+        return new Promise(async resolve => {
+            var file_template = sendData.file_template
+            // var subject = sendData.subject
+
+            let transporter = nodemailer.createTransport({
+                host: "smtp.gmail.com",
+                port: 465,
+                secure: true,
+                auth: {
+                    user: "test.user25112020@gmail.com",
+                    pass: "zmdjwxnvoamqwbgi"
+                    // noreply user + pass
+                }
+    
+            });
+
+            fs.readFile(file_template, { encoding: 'utf-8' }, function (err, html) {
+                
+                var template = handlebars.compile(html);
+                // console.log("🚀 ~ file: email.js ~ line 28 ~ sendData", sendData)
+                var htmlToSend = template(sendData);
+                
+                var mailOptions = {
+                    from: "test.user25112020@gmail.com", // set noreply mail id
+                    to: "test.user25112020@gmail.com", // set noreply mail id
+                    // subject: subject,
+                    html: htmlToSend
+                };
+
+                transporter.sendMail(mailOptions, (error, info) => {
+                    if (error) {
+                        console.log("error"+error)
+                        return ({ status: false, data: [], message: 'Could not send mail!' });
+                    }
+        
+                    console.log("info "+info)
+                    console.log('Message sent: %s', info.messageId);
+                    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+                    return ({ status: true, data: [], message: 'mail sent!.' });
+                });
+
+               
+            }); 
+
+        })
+
+    } catch (e) {
+        console.log(e)
+        return res.status(401).send({ "status": false, 'code': 401, "message": "Unable to send email!", data: {} })
+    }
+}
+
+// send welcome email
+async function sendWelcomeEmail(email, subject, html) {
+    try{
+        let transporter = nodemailer.createTransport({
+            host: "mail.privateemail.com",
+            port: 465,
+            secure: true,
+            auth: {
+                user: "hello@wizzcoin.io",
+                pass: "Wizz009$"
+            }
+
+        });
+        var mailOptions = {
+            // from:"test.user25112020@gmail.com",
+            from: "hello@wizzcoin.io",
+            to: email,
+            subject: subject,
+            html: html
+        };
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.log("error"+error)
+                return ({ status: false, data: [], message: 'Could not send mail!' });
+            }
+
+            console.log("info "+info)
+            console.log('Message sent: %s', info.messageId);
+            console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+            return ({ status: true, data: [], message: 'mail sent!.' });
+        });
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+
+module.exports = {
+    sendEmail,
+    sendWelcomeEmail
+}
